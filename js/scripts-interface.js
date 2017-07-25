4931638182af48f4573195cd17ae5506eb578f38
michaelkoceja <michael.koceja@gmail.com>
2017-07-25 08:40:34 -0700

    update headers, fix typo on module export

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,18 +1,20 @@
 var User = require('./../js/scripts.js').userModule;
 
-var displayDoctors = function(array) {
+var showDoctors = function(array) {
   array.forEach(function(data) {
-    $("#doctors").append('<p><h2>' + 'Dr. ' + data.firstName + ' ' + data.lastName + ' ' + '</h2><br>' + data.bio + '</p>');
+    $("#doctor-list").append('<p><h2>' + 'Dr. ' + data.firstName + ' ' + data.lastName + ' ' + '</h2><br>' + data.bio + '</p>');
   });
 };
 
+
+
 $(document).ready(function() {
-  $('#user-form').submit(function(event) {
+  $('form').submit(function(event) {
     event.preventDefault();
     var currentUser = new User();
     var symptom = $('#symptom').val();
 
-    currentUser.getDoctors(symptom, displayDoctors);
+    currentUser.getDoctors(symptom, showDoctors);
 
   });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 var apiKey = require('./../.env').apiKey;
 
-function User() {
-}
-
-var doctorArray = [];
-
 function Doctor(firstName, lastName, bio) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.bio = bio;
 }
 
-User.prototype.getDoctors = function(medicalIssue, displayDoctors) {
+function User() {
+}
+
+var doctorArray = [];
+
+User.prototype.getDoctors = function(medicalIssue, showDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
 
@@ -22,9 +22,9 @@ User.prototype.getDoctors = function(medicalIssue, displayDoctors) {
      });
 
 
-      console.log(result);
-      console.log(doctorArray);
-      displayDoctors(doctorArray);
+      // console.log(result);
+      // console.log(doctorArray);
+      showDoctors(doctorArray);
     })
    .fail(function(error){
       console.log("fail");
